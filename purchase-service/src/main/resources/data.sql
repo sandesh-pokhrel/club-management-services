68ef62d6586588e7f69b3206d1fd995d2ccf4d59
cold-tea <sandesh.pokhrel56@gmail.com>
2021-12-07 21:21:21 +0545

    insert statements for purchase added

--- purchase-service/src/main/resources/data.sql
+++ purchase-service/src/main/resources/data.sql
@@ -14,3 +14,12 @@ insert into purchase_sub_category (category_id, sub_category_name, base_rate, am
                                    appts, pays_on_completion) values (1, 'New Personal Training(36)', 12, 1638, 'Monthly', 5,
                                                                       30, true);
 
+insert into purchase_sub_category (category_id, sub_category_name, base_rate, amount, payment_interval, commission,
+                                   appts, pays_on_completion) values (2, 'Initial Consultation', 15, 155, 'Weekly', 5,
+                                                                      10, false);
+insert into purchase_sub_category (category_id, sub_category_name, base_rate, amount, payment_interval, commission,
+                                   appts, pays_on_completion) values (2, 'Annual Consulation Package', 670, 1638, 'Weekly', 6,
+                                                                      15, false);
+insert into purchase_sub_category (category_id, sub_category_name, base_rate, amount, payment_interval, commission,
+                                   appts, pays_on_completion) values (2, 'Annual Consultation', 25, 890, 'Weekly', 8,
+                                                                      20, true);
