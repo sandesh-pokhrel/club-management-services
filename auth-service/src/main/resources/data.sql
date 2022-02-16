a65f35e27f0959b3d7e98a6608bed4e0c92bd0fb
cold-tea <sandesh.pokhrel56@gmail.com>
2022-02-16 11:51:07 +0545

    auth service user_level insert statements updated

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -8,14 +8,28 @@ use club_management_auth;
 INSERT INTO oauth_client_details
 (client_id, resource_ids, client_secret, scope, authorized_grant_types,
  authorities, access_token_validity, refresh_token_validity, autoapprove)
-VALUES ('club_management_front', 'club_management_front', '$2a$12$QvOvgximnr/YN3SoE/ckrOWKyNLu3x7M3u4wQ9AzOCUgh3JgKmP4i',
+VALUES ('club_management_front', 'club_management_front',
+        '$2a$12$QvOvgximnr/YN3SoE/ckrOWKyNLu3x7M3u4wQ9AzOCUgh3JgKmP4i',
         'read,write', 'password,refresh_token,client_credentials,authorization_code',
         'TRUSTED_CLUB_MANAGEMENT_CLIENT', 300, 1800, 'true');
 
+insert into user_level (level_name, level_seniority)
+values ('Entry Level Consultant', 7),
+       ('Entry Level Trainer', 6),
+       ('Master Personal Trainer', 5),
+       ('Mid Level Consultant', 4),
+       ('Mid Level Trainer', 3),
+       ('Senior Level Consultant', 2),
+       ('Senior Level Trainer', 1);
+
 -- dummy username sandesh and password sandesh
-insert into auth_user values ('sandesh', '$2a$12$K89EqUvfERXAB0/diRrAb.OZdOLI2ZLpR1mTZysCB4qUspYj1e22y',
-                              'sandesh.pokhrel56@gmail.com', '9842100964','Sandesh', 'Pokhrel', 100, 500, 1);
-insert into auth_role (name, description) values ('ROLE_USER', 'Simple User - Has basic privileges');
-insert into auth_role (name, description) values ('ROLE_ADMIN', 'Admin User - Has permission to perform admin tasks');
-insert into user_role (username, role_id) values ('sandesh', 1);
+insert into auth_user
+values ('sandesh', '$2a$12$K89EqUvfERXAB0/diRrAb.OZdOLI2ZLpR1mTZysCB4qUspYj1e22y',
+        'sandesh.pokhrel56@gmail.com', '9842100964', 'Sandesh', 'Pokhrel', 100, 500, 1);
+insert into auth_role (name, description)
+values ('ROLE_USER', 'Simple User - Has basic privileges');
+insert into auth_role (name, description)
+values ('ROLE_ADMIN', 'Admin User - Has permission to perform admin tasks');
+insert into user_role (username, role_id)
+values ('sandesh', 1);
 COMMIT;
