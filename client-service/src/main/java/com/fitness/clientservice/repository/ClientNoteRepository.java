a36dd997b50640afd34040101dee1d4d0486b00c
Sandesh Pokhrel <sandesh.pokhrel56@gmail.com>
2022-02-25 17:43:54 +0545

    note update changes

--- client-service/src/main/java/com/fitness/clientservice/repository/ClientNoteRepository.java
+++ client-service/src/main/java/com/fitness/clientservice/repository/ClientNoteRepository.java
@@ -2,8 +2,17 @@ package com.fitness.clientservice.repository;
 
 import com.fitness.clientservice.model.ClientNote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface ClientNoteRepository extends JpaRepository<ClientNote, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE ClientNote cn SET cn.note = ?2 where cn.id = ?1")
+    void updateNoteById(Integer id, String note);
 }
