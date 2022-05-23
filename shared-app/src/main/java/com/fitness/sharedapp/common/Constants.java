6cb98435d26459d907c58e48c45e1ad6b0102671
Sandesh Pokhrel <sandesh.pokhrel56@gmail.com>
2022-05-23 09:07:47 +0545

    page size to 20

--- shared-app/src/main/java/com/fitness/sharedapp/common/Constants.java
+++ shared-app/src/main/java/com/fitness/sharedapp/common/Constants.java
@@ -1,7 +1,7 @@
 package com.fitness.sharedapp.common;
 
 public class Constants {
-    public static final Integer PAGE_SIZE = 3;
+    public static final Integer PAGE_SIZE = 20;
 
     public static final String[] DELETED_ALIKE_SCHEDULE_STATUS = {"CANCELLED", "NO_SHOW_NO_CHARGE"};
     public static final String[] COMPLETED_ALIKE_SCHEDULE_STATUS = {"COMPLETED", "NO_SHOW_CHARGE"};
